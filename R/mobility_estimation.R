bce5d258e4d0946dd736ed69cd7d7ded2e39e245
Clemens Schmid <clemens@nevrome.de>
2020-03-26 16:40:14 +0100

    added function mobility estimation

---
+++ R/mobility_estimation.R
@@ -0,0 +1,40 @@
+#' estimate_mobility
+#'
+#' @param spatial_origin test
+#' @param mobility_regions test
+#'
+#' @return test
+#'
+#' @export
+estimate_mobility <- function(spatial_origin, mobility_regions) {
+
+  points_regions <- spatial_origin %>%
+    dplyr::select(x, y, point_id) %>%
+    unique() %>%
+    sf::st_as_sf(
+      coords = c("x", "y"),
+      crs = "+proj=aea +lat_1=43 +lat_2=62 +lat_0=30 +lon_0=10 +x_0=0 +y_0=0 +ellps=intl +units=m +no_defs"
+    ) %>%
+    sf::st_intersection(mobility_regions) %>%
+    tibble::as_tibble() %>%
+    dplyr::select(-geometry)
+
+  pri <- spatial_origin %>%
+    dplyr::left_join(points_regions, by = "point_id")
+
+  pri_mean <- pri %>%
+    dplyr::group_by(
+      independent_table_id, kernel_setting_id, z, region_id
+    ) %>%
+    dplyr::summarise(
+      mean_km_per_decade = mean(spatial_distance)/1000/10,
+      mean_angle = mean(circular::circular(
+        angle_degree, type = "angles", units = "degrees", modulo = "2pi", template = 'geographics'),
+        na.rm = T
+      )
+    ) %>%
+    dplyr::ungroup()
+
+  return(pri_mean)
+
+}
