42870b9bc3f05c683118993ff8e114b762be84d9
Clemens Schmid <clemens@nevrome.de>
2020-03-26 14:32:09 +0100

    function to run interpolation on a model grid

--- R/model_grid.R
+++ R/model_grid.R
@@ -25,3 +25,50 @@ create_model_grid <- function(independent_tables, dependent_vars, kernel_setting
     ) %>% tibble::as_tibble()
 
 }
+
+#' run_model_grid
+#'
+#' @param model_grid test
+#' @param pred_grid test
+#'
+#' @return test
+#'
+#' @export
+run_model_grid <- function(model_grid, pred_grid) {
+
+  # run interpolation for each entry in the model_grid
+  prediction <- lapply(1:nrow(model_grid), function(i) {
+    interpolate_laGP(
+      independent = model_grid[["independent_table"]][[i]],
+      dependent = model_grid[["dependent_var"]][[i]],
+      pred_grid = pred_grid,
+      auto = model_grid[["kernel_setting"]][[i]][["auto"]],
+      d = model_grid[["kernel_setting"]][[i]][["d"]],
+      g = model_grid[["kernel_setting"]][[i]][["g"]]
+    )
+  })
+
+  # simplified model_grid
+  model_grid_simplified <- model_grid %>%
+    dplyr::mutate(independent_table_type = ifelse(independent_table_id == "age_center", "age_center", "age_sampled")) %>%
+    dplyr::select(-kernel_setting, -independent_table, -dependent_var)
+
+  # add prediction results for each run as a data.frame in a list column to model_grid
+  model_grid_simplified$prediction_sample <- lapply(prediction, function(x) {
+    pred <- data.frame(
+      point_id = 1:length(x$mean),
+      mean = x$mean,
+      sd = sqrt(x$s2),
+      stringsAsFactors = F
+    )
+    # merge with pred_grid to add relevant spatial information
+    dplyr::left_join(
+      pred,
+      pred_grid,
+      by = "point_id"
+    )
+  })
+
+  return(model_grid_simplified)
+
+}
