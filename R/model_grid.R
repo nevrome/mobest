0a94c88c3e4eb2301c772cfcefaa898c79ffd958
Clemens Schmid <clemens@nevrome.de>
2020-03-26 17:27:20 +0100

    allow model grid creation with variable prediction grids

--- R/model_grid.R
+++ R/model_grid.R
@@ -3,26 +3,30 @@
 #' @param independent_tables test
 #' @param dependent_vars test
 #' @param kernel_settings test
+#' @param pred_grids test
 #'
 #' @return test
 #'
 #' @export
-create_model_grid <- function(independent_tables, dependent_vars, kernel_settings) {
+create_model_grid <- function(independent_tables, dependent_vars, kernel_settings, pred_grids) {
 
   expand.grid(
-    kernel_setting_id = kernel_settings$kernel_setting_id,
-    dependent_var_id = c("PC1", "PC2", "PC3", "PC4"),
     independent_table_id = independent_tables$independent_table_id,
+    dependent_var_id = c("PC1", "PC2", "PC3", "PC4"),
+    kernel_setting_id = kernel_settings$kernel_setting_id,
+    pred_grid_id = pred_grids$pred_grid_id,
     stringsAsFactors = F
   ) %>%
-    dplyr::left_join(
-      kernel_settings, by = "kernel_setting_id"
-    ) %>%
     dplyr::left_join(
       independent_tables, by = "independent_table_id"
     ) %>% dplyr::mutate(
       dependent_var = lapply(dependent_var_id, function(x) { anno[[x]] })
-    ) %>% tibble::as_tibble()
+    ) %>% dplyr::left_join(
+      kernel_settings, by = "kernel_setting_id"
+    ) %>% dplyr::left_join(
+      pred_grids, by = "pred_grid_id"
+    ) %>%
+     tibble::as_tibble()
 
 }
 
