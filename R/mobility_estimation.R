2b7ffb903eb22dfd2fee5a6e37e4d7942c31eac1
Clemens Schmid <clemens@nevrome.de>
2020-04-07 18:03:21 +0200

    more stuff fit for packaging

--- R/mobility_estimation.R
+++ R/mobility_estimation.R
@@ -9,7 +9,7 @@
 estimate_mobility <- function(interpol_grid_origin, mobility_regions) {
 
   points_regions <- interpol_grid_origin %>%
-    dplyr::select(x, y, point_id) %>%
+    dplyr::select(.data[["x"]], .data[["y"]], .data[["point_id"]]) %>%
     unique() %>%
     sf::st_as_sf(
       coords = c("x", "y"),
@@ -17,20 +17,23 @@ estimate_mobility <- function(interpol_grid_origin, mobility_regions) {
     ) %>%
     sf::st_intersection(mobility_regions) %>%
     tibble::as_tibble() %>%
-    dplyr::select(-geometry)
+    dplyr::select(-.data[["geometry"]])
 
   ori <- interpol_grid_origin %>%
     dplyr::left_join(points_regions, by = "point_id")
 
   speed <- ori %>%
     dplyr::group_by(
-      independent_table_id, kernel_setting_id, z, region_id
+      .data[["independent_table_id"]],
+      .data[["kernel_setting_id"]],
+      .data[["z"]],
+      .data[["region_id"]]
     ) %>%
     dplyr::summarise(
-      mean_km_per_decade = mean(spatial_distance)/1000/10,
-      mean_x_to_origin = mean(x_to_origin),
-      mean_y_to_origin = mean(y_to_origin),
-      angle_deg = angle_between_along_360(c(mean_x_to_origin, mean_y_to_origin))
+      mean_km_per_decade = mean(.data[["spatial_distance"]])/1000/10,
+      mean_x_to_origin = mean(.data[["x_to_origin"]]),
+      mean_y_to_origin = mean(.data[["y_to_origin"]]),
+      angle_deg = angle_between_along_360(c(.data[["mean_x_to_origin"]], .data[["mean_y_to_origin"]]))
     ) %>%
     dplyr::ungroup()
 
@@ -39,6 +42,7 @@ estimate_mobility <- function(interpol_grid_origin, mobility_regions) {
 }
 
 angle_between_along_360 <- function(x) {
+
   a <- c(0,1)
   b <- x
 
@@ -49,4 +53,5 @@ angle_between_along_360 <- function(x) {
   } else{
     theta
   }
+
 }
