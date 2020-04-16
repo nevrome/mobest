4c65f45f0164ae8ecf866e64a799cc7ee42652b2
Clemens Schmid <clemens@nevrome.de>
2020-04-16 16:57:55 +0200

    the future is now

--- R/model_grid.R
+++ R/model_grid.R
@@ -56,7 +56,7 @@ run_model_grid <- function(model_grid, quiet = F) {
       dependent = model_grid[["dependent_var"]][[i]],
       pred_grid = model_grid[["pred_grid"]][[i]],
       # d has to be squared because of the configuration of the default laGP kernel
-      d = model_grid[["kernel_setting"]][[i]][["d"]],#^2,
+      d = model_grid[["kernel_setting"]][[i]][["d"]]^2,
       g = model_grid[["kernel_setting"]][[i]][["g"]],
       auto = model_grid[["kernel_setting"]][[i]][["auto"]],
       on_residuals = model_grid[["kernel_setting"]][[i]][["on_residuals"]]
