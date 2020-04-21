2f030b23f71033429511f2cd55fa399d95c33572
Clemens Schmid <clemens@nevrome.de>
2020-04-21 16:29:25 +0200

    crossvalidate function

--- R/crossvalidation.R
+++ R/crossvalidation.R
@@ -1,3 +1,110 @@
+#' crossvalidate
+#'
+#' @param independent test
+#' @param dependent test
+#' @param kernel test
+#'
+#' @export
+crossvalidate <- function(
+  independent,
+  dependent,
+  kernel
+) {
+
+  crossval <- cbind(independent, dependent %>% dplyr::bind_cols())
+
+  #### compile randomly reordered versions of crossval ####
+
+  crossval_mixed_list <- lapply(1:1, function(i) { crossval[sample(1:nrow(crossval), replace = F), ] })
+
+  #### run prediction test for each of this versions ####
+
+  lapply(crossval_mixed_list, function(crossval_mixed) {
+
+    #### split crossval into 10 sections ####
+
+    n <- 10
+    nr <- nrow(crossval_mixed)
+    crossval_10 <- split(crossval_mixed, rep(1:n, times = diff(floor(seq(0, nr, length.out = n + 1)))))
+
+    # 9 sections are used as a training dataset for the GP model
+    crossval_9_training <- lapply(
+      1:n, function(i) {
+        dplyr::bind_rows(crossval_10[-i])
+      }
+    )
+
+    # 1 section is used as a test dataset
+    crossval_9_test <- lapply(
+      1:n, function(i) {
+        crossval_10[[i]]
+      }
+    )
+
+    #### prepare model grid for current 9:1 comparison with different kernels ####
+
+    model_grid <- lapply(
+      1:n, function(i) {
+
+        # create model grid
+        model_grid <- mobest::create_model_grid(
+          list(training = crossval_9_training[[i]] %>% dplyr::select(x, y, z)),
+          dependent = lapply(
+            names(dependent), function(depvar) {
+              crossval_9_training[[i]][[depvar]]
+            }) %>%
+            stats::setNames(names(dependent)),
+          kernel = kernel,
+          prediction_grid = list(
+            crossval_9_test[[i]] %>%
+              dplyr::select(x, y, z) %>%
+              dplyr::mutate(point_id = 1:nrow(.))
+          ) %>% stats::setNames(i)
+        ) %>%
+          dplyr::select(-independent_table_type)
+
+      }
+    ) %>% dplyr::bind_rows()
+
+    #### run interpolation on model grid ####
+
+    model_grid_result <- mobest::run_model_grid(model_grid)
+
+    #### unnest prediction to get a point-wise prediction table ####
+
+    interpol_grid <- mobest::unnest_model_grid(model_grid_result)
+
+    #### merge prediction and real values ####
+
+    # make wide for mean and sd PC values
+    interpol_grid_wide <- interpol_grid %>% tidyr::pivot_wider(
+      names_from = "dependent_var_id",
+      values_from = c("mean", "sd")
+    )
+
+    # split by run training+test run setup to be able to merge with real test values
+    interpol_grid_wide_split <- interpol_grid_wide %>% split(interpol_grid_wide$pred_grid_id)
+
+    interpol_grid_merged <- lapply(
+      as.numeric(unique(interpol_grid_wide$pred_grid_id)), function(i) {
+        interpol_grid_wide_split[[i]] %>%
+          dplyr::left_join(
+            crossval_9_test[[i]] %>%
+              dplyr::select(names(dependent)) %>%
+              dplyr::mutate(point_id = 1:nrow(.)),
+            by = "point_id"
+          )
+      }
+    ) %>% dplyr::bind_rows()
+
+    return(interpol_grid_merged)
+
+  }) %>% dplyr::bind_rows() -> interpol_grid_merged_all
+
+  return(interpol_grid_merged_all)
+
+}
+
 #' create_kernel_grid
 #'
 #' @param ds test
