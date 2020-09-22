d2f8636e95e7f1ebe3014765e5d66fbb0dbb5b70
Clemens Schmid <clemens@nevrome.de>
2020-09-22 10:49:50 +0200

    mobility region attribution to the prediction grid at the beginning of the pipeline

--- R/pred_grid.R
+++ R/pred_grid.R
@@ -5,6 +5,8 @@
 #'
 #' @param area An object of class \code{sf}. Polygons where the spatial grid should
 #' be constructed
+#' @param mobility_regions An object of class \code{sf}. Polygons of regions the grid points
+#' should be attributed to
 #' @param spatial_cell_size Numeric. Size of the spatial grid cells in the unit of
 #' \code{area}. See \code{?sf::st_make_grid} for more info
 #' @param time_layers Numeric vector. Temporal layers of the requested spatiotemporal
@@ -14,21 +16,24 @@
 #' point identifier column point_id
 #'
 #' @export
-create_prediction_grid <- function(area, spatial_cell_size = 100000, time_layers = seq(-7500, -500, 100)) {
+create_prediction_grid <- function(area, mobility_regions, spatial_cell_size = 100000, time_layers = seq(-7500, -500, 100)) {
 
-  pred_points_space <- area %>%
+  point_grid <- area %>%
     sf::st_make_grid(cellsize = spatial_cell_size, what = "centers") %>%
-    sf::st_intersection(area) %>%
-    sf::st_coordinates() %>%
-    tibble::as_tibble() %>%
-    dplyr::rename(x = .data[["X"]], y = .data[["Y"]])
+    sf::st_sf() %>%
+    sf::st_join(mobility_regions, join = sf::st_intersects) %>%
+    dplyr::mutate(
+      x = sf::st_coordinates(.)[,1],
+      y = sf::st_coordinates(.)[,2]
+    ) %>%
+    sf::st_drop_geometry()
 
-  time_layers <- tibble::tibble(
+  time_grid <- tibble::tibble(
     z = time_layers
   )
 
-  pred_grid <- pred_points_space %>%
-    tidyr::crossing(time_layers) %>%
+  pred_grid <- point_grid %>%
+    tidyr::crossing(time_grid) %>%
     dplyr::mutate(
       point_id = 1:nrow(.)
     )
