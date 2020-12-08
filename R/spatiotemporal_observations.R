d217a4b1365511b3f9e27dea0d8bb30998c02c99
Clemens Schmid <clemens@nevrome.de>
2020-12-08 16:13:21 +0100

    more love for the spatiotemporal observation class methods

--- R/spatiotemporal_observations.R
+++ R/spatiotemporal_observations.R
@@ -1,5 +1,35 @@
-create_spatpos <- function(id, x, y, z, name) {
-  # input check and modifications
+mean_spatpos_uncertain <- function(spatpos_uncertain) {
+  # input check
+  checkmate::assert_class(spatpos_uncertain, "mobest_uncertain_spatiotemporalpositions")
+  checkmate::assert_true(length(spatpos_uncertain) > 1)
+  # calculate mean
+  purrr::reduce(
+    spatpos_uncertain, function(a, b) {
+      create_spatpos(
+        id = a$id,
+        x = a$x+b$x,
+        y = a$y+b$y,
+        z = a$z+b$z
+      )
+    }
+  ) %>% dplyr::mutate(
+    dplyr::across(c("x", "y", "z"), function(x) { x/length(spatpos_uncertain) })
+  )
+}
+
+create_spatpos <- function(id, x, y, z) {
+  # input check
+  checkmate::assert_vector(id)
+  checkmate::assert_numeric(x)
+  checkmate::assert_numeric(y)
+  checkmate::assert_numeric(z)
+  # compile tibble
+  tibble::tibble(id = id, x = x, y = y, z = z) %>%
+    tibble::new_tibble(., nrow = nrow(.), class = "mobest_spatiotemporalpositions")
+}
+
+create_spatpos_uncertain <- function(id, x, y, z, name) {
+  # input check
   checkmate::assert_vector(id)
   checkmate::assert_list(x)
   checkmate::assert_list(y)
@@ -18,16 +48,11 @@ create_spatpos <- function(id, x, y, z, name) {
   ) %>%
     purrr::pmap(
       function(id, x, y, z) {
-        tibble::tibble(
-          id = id,
-          x = x,
-          y = y,
-          z = z
-        )
+        create_spatpos(id = id, x = x, y = y, z = z)
       }
     ) %>%
     magrittr::set_names(name) %>%
-    magrittr::set_class("mobest_spatiotemporalposition")
+    magrittr::set_class("mobest_uncertain_spatiotemporalpositions")
 }
 
 create_obs <- function(id, ...) {
@@ -41,6 +66,6 @@ create_obs <- function(id, ...) {
     id = id
   ) %>%
     cbind(...) %>%
-    magrittr::set_class("mobest_observation")
+    magrittr::set_class("mobest_observations")
 }
 
