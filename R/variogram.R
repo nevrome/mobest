a6254c0541f38dd827040178bee1f82c024a0593
Clemens Schmid <clemens@nevrome.de>
2020-12-14 15:48:05 +0100

    unified input definition for model_grid and variogram

--- R/variogram.R
+++ R/variogram.R
@@ -7,18 +7,16 @@
 #'
 #' @export
 calculate_pairwise_distances <- function(independent, dependent) {
-  # input check
+  # input check and transformation
   checkmate::assert_class(independent, "mobest_spatiotemporalpositions")
-  checkmate::assert_class(dependent, "mobest_observations")
+  dependent <- dplyr::bind_cols(dependent)
   if (nrow(independent) != nrow(dependent)) {
     stop("independent and dependent must have the same number of rows")
   }
-  if (length(unique(independent$id)) > 1) {
-    "independent must not be uncertain (only one iteration)"
-  }
+  ids <- independent$id
   # geo distance
   d_geo <- dist(independent %>% dplyr::select(.data[["x"]], .data[["y"]]), "euclidean") %>% as.matrix()
-  rownames(d_geo) <- colnames(d_geo) <- independent$id
+  rownames(d_geo) <- colnames(d_geo) <- ids
   d_geo_long <- d_geo %>%
     reshape2::melt(value.name = "geo_dist") %>%
     dplyr::mutate(
@@ -27,11 +25,11 @@ calculate_pairwise_distances <- function(independent, dependent) {
     )
   # time distance
   d_time <- dist(independent %>% dplyr::select(.data[["z"]]), "euclidean") %>% as.matrix()
-  rownames(d_time) <- colnames(d_time) <- independent$id
+  rownames(d_time) <- colnames(d_time) <- ids
   d_time_long <- d_time %>% reshape2::melt(value.name = "time_dist")
   # obs total distance
   d_obs_total <- dist(dependent %>% dplyr::select(-.data[["id"]]), "euclidean") %>% as.matrix()
-  rownames(d_obs_total) <- colnames(d_obs_total) <- dependent$id
+  rownames(d_obs_total) <- colnames(d_obs_total) <- ids
   d_obs_total_long <- d_obs_total %>% reshape2::melt(value.name = "obs_dist_total")
   # obs individual distance
   var_names <- get_var_names(dependent)
@@ -39,13 +37,13 @@ calculate_pairwise_distances <- function(independent, dependent) {
     purrr::map(function(var_name) {
       # d_obs
       d_obs <- dist(dependent[[var_name]], "euclidean") %>% as.matrix()
-      rownames(d_obs) <- colnames(d_obs) <- dependent$id
+      rownames(d_obs) <- colnames(d_obs) <- ids
       d_obs_long <- d_obs %>% reshape2::melt(value.name = paste0(var_name, "_dist"))
       # d_obs_resid
       model <- lm(dependent[[var_name]] ~ independent$x + independent$y + independent$z)
       model_residuals <- residuals(model)
       d_obs_resid <- as.matrix(dist(model_residuals, "euclidean"))
-      rownames(d_obs_resid) <- colnames(d_obs_resid) <- dependent$id
+      rownames(d_obs_resid) <- colnames(d_obs_resid) <- ids
       d_obs_resid_long <- d_obs_resid %>% reshape2::melt(value.name = paste0(var_name, "_dist_resid"))
       # combine
       dplyr::full_join(
