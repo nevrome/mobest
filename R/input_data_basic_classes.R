bbf72ac45530efc21c42ac4c6adec898b0946d6d
Clemens Schmid <clemens@nevrome.de>
2022-04-28 14:33:56 +0200

    cleaning input data constructor code

--- R/input_data_basic_classes.R
+++ R/input_data_basic_classes.R
@@ -1,25 +1,138 @@
 #' Input data type constructors
 #'
 #' Functions to create the main input data types for the mobest package.
-#' See the README for example code.
 #'
-#' @param id Vector. IDs of the observation points
+#' @param id Vector. Identifiers of the individual data points
 #' @param x Numeric vector. Spatial x-axis coordinates
 #' @param y Numeric vector. Spatial y-axis coordinates
 #' @param z Numeric vector. Temporal positions
-#' @param dsx Double. Kernel lengthscale parameter for the x dimension (spatial x-axis). See \code{?laGP::newGP} for more info
+#' @param area An object of class \code{sf}. Polygons where the spatial grid should
+#' be constructed
+#' @param spatial_cell_size Numeric. Size of the output spatial grid cells in the unit of
+#' \code{area}. See \code{?sf::st_make_grid} for more info
+#' @param dsx Double. Kernel lengthscale parameter for the x dimension (spatial x-axis). See \code{?laGP::newGP} for more information
 #' @param dsy Double. Kernel lengthscale parameter for the y dimension (spatial y-axis)
 #' @param dt Double. Kernel lengthscale parameter for the z dimension (temporal axis)
 #' @param g Double. Kernel nugget parameter
-#' @param on_residuals Logical. In the field calculation down the pipeline: Should a linear model take out the main trends before the kriging interpolation?
-#' @param auto Logical. In the field calculation down the pipeline:
+#' @param on_residuals Logical. In the field calculation:
+#' Should a linear model take over the main trends before the kriging interpolation?
+#' @param auto Logical. In the field calculation:
 #' Should the lengthscale and nugget values be automatically determined by laGPs
 #' maximum likelihood algorithm? See \code{?laGP::mleGPsep} for more info
-#' @param ... Different inputs (see examples in README)
+#' @param ... Different inputs - usually individual elements to be merged in a list type
 #' @param .names Vector. Names of different object iterations
 #'
-#' @return Different data types for specific applications.
+#' @return Different data types
 #'
+#' @name input_data_constructors
+NULL
+
+#### spatial coordinates ####
+
+#' @rdname input_data_constructors
+#' @export
+create_geopos <- function(id, x, y, ...) {
+  # input check
+  checkmate::assert_atomic_vector(id, any.missing = F, unique = T)
+  checkmate::assert_numeric(x)
+  checkmate::assert_numeric(y)
+  checkmate::assert_true(
+    purrr::map_int(list(id, x, y, ...), length) %>% unique %>% length %>% magrittr::equals(1)
+  )
+  # compile tibble
+  tibble::tibble(id = id, x = x, y = y, ...) %>%
+    tibble::new_tibble(., nrow = nrow(.), class = "mobest_spatialpositions")
+}
+
+#' @rdname input_data_constructors
+#' @export
+create_prediction_grid <- function(area, spatial_cell_size) {
+  # dependency check
+  check_if_packages_are_available("sf")
+  # input checks
+  checkmate::assert_class(area, classes = "sf")
+  # prepare grid
+  sf::st_agr(area) <- "constant"
+  space_grid_rect_sf <- area %>%
+    sf::st_make_grid(cellsize = spatial_cell_size, what = "centers") %>%
+    sf::st_sf()
+  sf::st_agr(space_grid_rect_sf) <- "constant"
+  space_grid_sf <- sf::st_intersection(space_grid_rect_sf, area)
+  space_grid <- space_grid_sf %>%
+    dplyr::mutate(
+      x = sf::st_coordinates(.)[,1],
+      y = sf::st_coordinates(.)[,2]
+    ) %>%
+    sf::st_drop_geometry() %>%
+    dplyr::select(.data[["x"]], .data[["y"]])
+  # compile output
+  mobest::create_geopos(
+    id = 1:nrow(space_grid),
+    x = space_grid$x,
+    y = space_grid$y
+  )
+}
+
+#' @rdname input_data_constructors
+#' @export
+create_geopos_multi <- function(..., .names = NULL) {
+  tibble_multi_function_factory(
+    "mobest_spatialpositions",
+    "mobest_spatialpositions_multi",
+    T,T
+  )(..., .names = .names)
+}
+
+#### spatiotemporal coordinates ####
+
+#' @rdname input_data_constructors
+#' @export
+geopos_to_spatpos <- function(geopos, z) {
+  # input check
+  checkmate::assert_class(geopos, classes = "mobest_spatialpositions")
+  checkmate::assert_numeric(z)
+  # expand grid
+  spatpos <- geopos %>%
+    tidyr::crossing(tibble::tibble(z = z))
+  # compile output
+  mobest::create_spatpos(
+    id = 1:nrow(spatpos),
+    x = spatpos$x,
+    y = spatpos$y,
+    z = spatpos$z,
+    geo_id = spatpos$id
+  ) %>%
+    dplyr::bind_cols(spatpos %>% dplyr::select(-id, -x, -y, -z))
+}
+
+#' @rdname input_data_constructors
+#' @export
+create_spatpos <- function(id, x, y, z, ...) {
+  # input check
+  checkmate::assert_atomic_vector(id, any.missing = F, unique = T)
+  checkmate::assert_numeric(x)
+  checkmate::assert_numeric(y)
+  checkmate::assert_numeric(z)
+  checkmate::assert_true(
+    purrr::map_int(list(id, x, y, z, ...), length) %>% unique %>% length %>% magrittr::equals(1)
+  )
+  # compile tibble
+  tibble::tibble(id = id, x = x, y = y, z = z, ...) %>%
+    tibble::new_tibble(., nrow = nrow(.), class = "mobest_spatiotemporalpositions")
+}
+
+#' @rdname input_data_constructors
+#' @export
+create_spatpos_multi <- function(..., .names = NULL) {
+  tibble_multi_function_factory(
+    "mobest_spatiotemporalpositions",
+    "mobest_spatiotemporalpositions_multi",
+    T,T
+  )(..., .names = .names)
+}
+
+#### genetic coordinates ####
+
 #' @rdname input_data_constructors
 #' @export
 create_obs <- function(..., .names = NULL) {
@@ -86,76 +199,7 @@ create_obs_obserror_multi <- function(..., .names = NULL) {
   )(..., .names = .names)
 }
 
-#' @rdname input_data_constructors
-#' @export
-create_geopos <- function(id, x, y, ...) {
-  # input check
-  checkmate::assert_atomic_vector(id, any.missing = F, unique = T)
-  checkmate::assert_numeric(x)
-  checkmate::assert_numeric(y)
-  checkmate::assert_true(
-    purrr::map_int(list(id, x, y, ...), length) %>% unique %>% length %>% magrittr::equals(1)
-  )
-  # compile tibble
-  tibble::tibble(id = id, x = x, y = y, ...) %>%
-    tibble::new_tibble(., nrow = nrow(.), class = "mobest_spatialpositions")
-}
-
-#' @rdname input_data_constructors
-#' @export
-create_geopos_multi <- function(..., .names = NULL) {
-  tibble_multi_function_factory(
-    "mobest_spatialpositions",
-    "mobest_spatialpositions_multi",
-    T,T
-  )(..., .names = .names)
-}
-
-#' @rdname input_data_constructors
-#' @export
-geopos_to_spatpos <- function(geopos, z) {
-  # input check
-  checkmate::assert_class(geopos, classes = "mobest_spatialpositions")
-  checkmate::assert_numeric(z)
-  # expand grid
-  spatpos <- geopos %>%
-    tidyr::crossing(tibble::tibble(z = z))
-  # compile output
-  mobest::create_spatpos(
-    id = 1:nrow(spatpos),
-    x = spatpos$x,
-    y = spatpos$y,
-    z = spatpos$z,
-    geo_id = spatpos$id
-  ) %>%
-    dplyr::bind_cols(spatpos %>% dplyr::select(-id, -x, -y, -z))
-}
-
-#' @rdname input_data_constructors
-#' @export
-create_spatpos <- function(id, x, y, z, ...) {
-  # input check
-  checkmate::assert_atomic_vector(id, any.missing = F, unique = T)
-  checkmate::assert_numeric(x)
-  checkmate::assert_numeric(y)
-  checkmate::assert_numeric(z)
-  checkmate::assert_true(
-    purrr::map_int(list(id, x, y, z, ...), length) %>% unique %>% length %>% magrittr::equals(1)
-  )
-  # compile tibble
-  tibble::tibble(id = id, x = x, y = y, z = z, ...) %>%
-    tibble::new_tibble(., nrow = nrow(.), class = "mobest_spatiotemporalpositions")
-}
-
-#' @rdname input_data_constructors
-#' @export
-create_spatpos_multi <- function(..., .names = NULL) {
-  tibble_multi_function_factory(
-    "mobest_spatiotemporalpositions",
-    "mobest_spatiotemporalpositions_multi",
-    T,T
-  )(..., .names = .names)
-}
+#### kernel settings ####
 
 #' @rdname input_data_constructors
 #' @export
