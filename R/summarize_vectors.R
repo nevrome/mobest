f9fce2ce7eb10222b315b0f29f3e9f234a27e1aa
Clemens Schmid <clemens@nevrome.de>
2022-05-01 22:32:10 +0200

    more work on the origin vector summary functions

--- R/summarize_vectors.R
+++ R/summarize_vectors.R
@@ -1,11 +1,12 @@
 #' Summarize the results of the origin search
 #'
-#' Functions to transform the large origin grid objects to meaningful summary datasets
+#' Functions to transform the large origin vector tables to meaningful moving window
+# ' summaries
 #'
-#' @param origin_grid An object of class \code{mobest_origingrid} as created by
-#' \link{search_spatial_origin}
-#' @param moving_origin_grid An object of class \code{mobest_movingorigingrid}
-#' as created by \link{average_origin_moving_window}
+#' @param origin_vectors An object of class \code{mobest_originvectors} as created by
+#' \link{determine_origin_vectors}
+#' @param moving_origin_grid An object of class \code{mobest_originsummary}
+#' as created by \link{summarize_origin_vectors}
 #' @param window_start Start date of the moving window sequence
 #' @param window_stop Stop date of the moving window sequence
 #' @param window_width Width of each individual moving window
@@ -13,13 +14,14 @@
 #' If the first window starts at -3500 and extends until -3200, should the next
 #' one start at -3450 or -3400, so with window_step = 50 or window_step = 100?
 #'
-#' @return Different data products: \code{mobest_meanorigingrid},
-#' \code{mobest_movingorigingrid} or \code{mobest_origingridnodatawindows}
+#' @return \link{summarize_origin_vectors} returns and object of class
+#' \code{mobest_originsummary}, \link{find_no_data_windows} then
+#' \code{mobest_nodatawindows}.
 #'
-#' @name average_origin
+#' @name origin_summary
 NULL
 
-#' @rdname average_origin
+#' @rdname origin_summary
 #' @export
 summarize_origin_vectors <- function(
   origin_vectors, ..., window_start, window_stop, window_width, window_step
@@ -27,6 +29,10 @@ summarize_origin_vectors <- function(
   .grouping_var <- rlang::ensyms(...)
   # input check
   checkmate::assert_class(origin_vectors, "mobest_originvectors")
+  checkmate::assert_number(window_start)
+  checkmate::assert_number(window_stop, lower = window_start)
+  checkmate::assert_number(window_width)
+  checkmate::assert_number(window_step)
   # split vector groups
   vector_groups <- origin_vectors %>%
     dplyr::group_split(
@@ -34,7 +40,7 @@ summarize_origin_vectors <- function(
     )
   # loop through units
   future::plan(future::multisession)
-  vector_summary <- furrr::future_map_dfr(
+  origin_summary <- furrr::future_map_dfr(
     vector_groups,
     function(vector_group) {
       purrr::map2_df(
@@ -68,7 +74,7 @@ summarize_origin_vectors <- function(
                     mean(.data[["field_y"]] - .data[["search_y"]])^2
                 ),
                 se_spatial_distance = if (nrow(io_run_grouped) >= 3) {
-                  se(io_run_grouped$mean_spatial_distance)
+                  calculate_standard_error(io_run_grouped$mean_spatial_distance)
                 } else {
                   Inf
                 },
@@ -91,7 +97,6 @@ summarize_origin_vectors <- function(
               ) %>%
               dplyr::summarise(
                 z = mean(c(start, end)),
-                region_id = region,
                 undirected_mean_spatial_distance = NA,
                 directed_mean_spatial_distance = NA,
                 mean_angle_deg = NA,
@@ -103,17 +108,22 @@ summarize_origin_vectors <- function(
       )
     }
   )
-  vector_summary %>%
+  origin_summary %>%
     tibble::new_tibble(., nrow = nrow(.), class = "mobest_originsummary")
 }
 
-se <- function(x) stats::sd(x)/sqrt(length(x))
-
-#' @rdname average_origin
+#' @rdname origin_summary
 #' @export
-no_data_windows <- function(moving_origin_grid, window_step) {
-  moving_origin_grid %>%
-    dplyr::group_by(.data[["region_id"]]) %>%
+find_no_data_windows <- function(origin_summary, ...) {
+  .grouping_var <- rlang::ensyms(...)
+  # input check
+  checkmate::assert_class(origin_summary, "mobest_originsummary")
+  # find windows
+  window_step <- abs(origin_summary$z[1] - origin_summary$z[2])
+  origin_summary %>%
+    dplyr::group_by(
+      !!!.grouping_var
+    ) %>%
     dplyr::mutate(
       usd = tidyr::replace_na(.data[["undirected_mean_spatial_distance"]], 0),
       cumsum_undir_dist = cumsum(.data[["usd"]])
@@ -121,12 +131,19 @@ no_data_windows <- function(moving_origin_grid, window_step) {
     dplyr::filter(
       is.na(.data[["undirected_mean_spatial_distance"]])
     ) %>%
-    dplyr::group_by(.data[["region_id"]], .data[["cumsum_undir_dist"]]) %>%
+    dplyr::group_by(
+      !!!.grouping_var,
+      .data[["cumsum_undir_dist"]]
+    ) %>%
     dplyr::summarise(
       min_date_not_covered = min(.data[["z"]]) - window_step,
       max_date_not_covered = max(.data[["z"]]) + window_step,
       .groups = "drop"
     ) %>%
     dplyr::select(-.data[["cumsum_undir_dist"]]) %>%
-    tibble::new_tibble(., nrow = nrow(.), class = "mobest_origingridnodatawindows")
+    tibble::new_tibble(., nrow = nrow(.), class = "mobest_nodatawindows")
 }
+
+#### helper functions ####
+
+calculate_standard_error <- function(x) { stats::sd(x)/sqrt(length(x)) }
