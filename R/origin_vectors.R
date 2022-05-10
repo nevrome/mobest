e03cd915acd1d55288b7c5397cfae6bfd4be1654
Clemens Schmid <clemens@nevrome.de>
2022-05-10 21:23:59 +0200

    angle_cut is unimportant and can be added later

--- R/origin_vectors.R
+++ R/origin_vectors.R
@@ -37,8 +37,7 @@ determine_origin_vectors <- function(
         # keep only the maximum probability grid cell
         dplyr::slice_max(.data[["probability"]], n = 1, with_ties = FALSE) %>%
         dplyr::mutate(
-          ov_angle_deg = vec2deg(c(.data[["ov_x"]], .data[["ov_y"]])),
-          ov_angle_cut = cut_angle_deg(.data[["ov_angle_deg"]])
+          ov_angle_deg = vec2deg(c(.data[["ov_x"]], .data[["ov_y"]]))
         )
       }
     )
